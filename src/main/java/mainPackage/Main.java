b07d29bce3df58e049a36227299870949e0aea34
Adib Farhan <adib.rcc2710@gmail.com>
2023-09-03 16:59:45 +0600

    edited Main.java

--- src/main/java/mainPackage/Main.java
+++ src/main/java/mainPackage/Main.java
@@ -13,7 +13,7 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("loginPage.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 1000, 700);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.setMinHeight(700);
