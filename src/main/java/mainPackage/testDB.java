2d616c16e8182f5dfe377681bfb9f0c98c6b3a95
Rizwan Rabbi <rizwanrabbir2@gmail.com>
2023-09-03 14:36:19 +0600

    blyeat

---
+++ src/main/java/mainPackage/testDB.java
@@ -0,0 +1,8 @@
+package mainPackage;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class testDB {
+
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,7 @@ module com.example.demo {
     requires javafx.controls;
     requires javafx.fxml;
 
-
+    requires java.sql;
     opens mainPackage to javafx.fxml;
     exports mainPackage;
 }
