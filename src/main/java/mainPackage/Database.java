94448d4e49b6e70abbbfaa5f8a7cdfb38d71b1e2
Rizwan Rabbi <rizwanrabbir2@gmail.com>
2023-09-03 15:32:17 +0600

    Started to work in LoginStuff
    Signed-off-by: Rizwan Rabbi <rizwanrabbir2@gmail.com>

--- src/main/java/mainPackage/Database.java
+++ src/main/java/mainPackage/Database.java
@@ -2,4 +2,15 @@ package mainPackage;
 
 public class Database {
 
+    public static String hash(String s)
+    {
+        StringBuilder ret = new StringBuilder();
+        for(int i = 0; i < s.length(); i++)
+        {
+            ret.append((char)(s.charAt(i) ^ 4321));
+        }
+        return String.valueOf(ret);
+    }
+
+
 }
