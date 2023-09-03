a28279b2e3bafe2dfdf3d5503ba70ac467552b99
Rizwan Rabbi <rizwanrabbir2@gmail.com>
2023-09-04 01:22:00 +0600

    cant make the hash work but but finished upto login
    Signed-off-by: Rizwan Rabbi <rizwanrabbir2@gmail.com>

--- src/main/java/mainPackage/LoginPageController.java
+++ src/main/java/mainPackage/LoginPageController.java
@@ -2,28 +2,42 @@ package mainPackage;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
 import java.io.IOException;
+import java.sql.SQLException;
+import java.util.Objects;
 
 public class LoginPageController {
     @FXML
     public TextField emailBox;
     @FXML
     public TextField passwordBox;
-
+    @FXML
+    public Label warningBox;
     @FXML
     protected void signUpButtonClick (ActionEvent e) throws IOException {
 //        SceneChanger.changeTo("",e);
     }
     @FXML
-    protected void loginButtonClick (ActionEvent e)
-    {
+    protected void loginButtonClick (ActionEvent e) throws SQLException, IOException {
         String email = emailBox.getText();
         String pword = Database.hash(passwordBox.getText());
+        System.out.println(pword);
 
-
-        //  verification;
+        UserInfo userInfo = Database.getUserInfo(email);
+        if(pword.equals(userInfo.getPassword()))
+        {
+            HomePageController.userInfo = userInfo;
+//            will change to HomePage
+//            SceneChanger.changeTo("HomePage.fxml",e);
+        }
+        else
+        {
+            warningBox.setText("Incorrect E-mail/ Password");
+        }
     }
 
 
